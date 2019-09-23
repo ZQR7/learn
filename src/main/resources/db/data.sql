76e4341eea6878c7d67cd0f599c7ab40a7d5a148
ZQR7 <2397721424@qq.com>
2019-09-23 20:34:18 +0800

    update the data sql file

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,5 +6,5 @@ INSERT INTO `user` (id, username, password, role) VALUES ('5', 'user3', '1234', 
 INSERT INTO `user` (id, username, password, role) VALUES ('6', 'user4', '1234', 'user');
 
 INSERT INTO `news` (title,content) VALUES ('xin wen biao ti','nei rong');
-INSERT INTO `news` (title,content) VALUES ('it官网打折中','it官网正在搞品牌打折活动');
+INSERT INTO `news` (title,content) VALUES ('it官网打折','it官网正在搞品牌打折活动');
 COMMIT;
